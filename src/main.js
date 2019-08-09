66021055506b0cc5cdcd2a326b6facf23275fc10
yk <1586500126@qq.com>
2019-08-09 16:56:31 +0800

    完成公告位

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/theme-chalk/display.css';
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import "babel-polyfill"
 // require styles
 import 'swiper/dist/css/swiper.css'
 
--- src/pages/home/components/adve.vue
+++ src/pages/home/components/adve.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="adve">
-      <el-row :span="20" type="flex"  justify="center " >
-          <el-col :xs="24"  :sm="24"  :md="24" :lg="24" :xl="24">
-            <el-col :xs="12"  :sm="5"  :md="5" :lg="5" :xl="5" >
+      <el-row :span="24"  type="flex"  justify="center " class="adve-wrap">
+            <el-col class="adve-col" :xs="10"  :sm="10"  :md="5" :lg="5" :xl="5" >
                 <img src="@/assets/img/ad-1.jpg" alt="">
             </el-col>
-            <el-col :xs="12"  :sm="5"  :md="5" :lg="5" :xl="5">
+            <el-col class="adve-col" :xs="10"  :sm="10"  :md="5" :lg="5" :xl="5">
                 <img src="@/assets/img/ad-2.jpg" alt="">
             </el-col>
-            <el-col :xs="12"  :sm="5"  :md="5" :lg="5" :xl="5">
+            <el-col class="adve-col" :xs="10"  :sm="10"  :md="5" :lg="5" :xl="5">
                 <img src="@/assets/img/ad-3.jpg" alt="">
             </el-col>
-            <el-col :xs="12"  :sm="5"  :md="5" :lg="5" :xl="5">
+            <el-col class="adve-col" :xs="10"  :sm="10"  :md="5" :lg="5" :xl="5">
                 <img src="@/assets/img/ad-4.jpg" alt="">
             </el-col>
-          </el-col>
       </el-row>
   </div>
 </template>
@@ -27,9 +25,17 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+@import "../../../assets/style/media.css";
+
     .adve
         width 100%
+        .adve-wrap .adve-col
+            margin 20px
         img 
             width 100%
             border-radius 10px
+            
+        .adve-wrap
+            flex-wrap wrap
+            
 </style>
